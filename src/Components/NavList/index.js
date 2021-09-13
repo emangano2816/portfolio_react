b7ed43ee348f5a0994a57578e07488c6f0ddc134
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-13 15:11:23 -0400

    Updated resume; updated link to resume; removed old resume.

--- src/Components/NavList/index.js
+++ src/Components/NavList/index.js
@@ -14,7 +14,7 @@ function NavList() {
                 <a href={process.env.PUBLIC_URL +"/#contact-me"}>Contact Me</a>
             </li>
             <li>
-                <a href={process.env.PUBLIC_URL +"/assets/Resume_Coding_20210812.pdf"} target="_blank" rel="noreferrer noopener" >Resume</a>
+                <a href={process.env.PUBLIC_URL +"/assets/Resume_Coding_20210913.pdf"} target="_blank" rel="noreferrer noopener" >Resume</a>
             </li>
         </ul> 
     )
