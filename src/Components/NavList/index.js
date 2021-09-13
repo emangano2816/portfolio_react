367035f8fc195eb3d146c4ad9c7d454f3b4290e7
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-13 14:42:29 -0400

    Changed settingin 404.html; added process.env.PUBLIC_URL inside anchors.

--- src/Components/NavList/index.js
+++ src/Components/NavList/index.js
@@ -5,13 +5,13 @@ function NavList() {
     return (
         <ul>
             <li>
-                <a href="/#aboutme">About Me</a>
+                <a href={process.env.PUBLIC_URL +"/#aboutme"}>About Me</a>
             </li>
             <li>
-                <a href="/#work">Work</a>
+                <a href={process.env.PUBLIC_URL +"/#work"}>Work</a>
             </li>
             <li>
-                <a href="/#contact-me">Contact Me</a>
+                <a href={process.env.PUBLIC_URL +"/#contact-me"}>Contact Me</a>
             </li>
             <li>
                 <a href={process.env.PUBLIC_URL +"/assets/Resume_Coding_20210812.pdf"} target="_blank" rel="noreferrer noopener" >Resume</a>
