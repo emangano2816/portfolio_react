825e038f9c4e37714c43f873cfc5d5c65c533a88
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-13 13:22:36 -0400

    Changed homepage; added switch to router.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from '../src/Components/pages/Home';
 import ShareSpace from './Components/pages/ShareSpace'
 import MovieDrinkMatch from "./Components/pages/MovieDrinkMatch";
@@ -10,15 +10,15 @@ import WorkdayScheduler from "./Components/pages/WorkdayScheduler";
 function App() {
   return (
     <Router>
-      <div>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/sharespace' component={ShareSpace} />
-          <Route exact path='/empdirreact' component={EmpDirReact} />
-          <Route exact path='/moviedrinkmatch' component={MovieDrinkMatch} />
-          <Route exact path='/codequiz' component={CodeQuiz} />
-          <Route exact path='/weatherdashboard' component={WeatherDashboard} />
-          <Route exact path='/workdayscheduler' component={WorkdayScheduler} />
-      </div>
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/sharespace' component={ShareSpace} />
+            <Route exact path='/empdirreact' component={EmpDirReact} />
+            <Route exact path='/moviedrinkmatch' component={MovieDrinkMatch} />
+            <Route exact path='/codequiz' component={CodeQuiz} />
+            <Route exact path='/weatherdashboard' component={WeatherDashboard} />
+            <Route exact path='/workdayscheduler' component={WorkdayScheduler} />
+        </Switch>
     </Router>
       
 
