78fe9de94e6e71871b4afb45cee8fe38f3605fcf
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-12 20:36:28 -0400

    Added component/pages/route for weather dashboard project details.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from '../src/Components/pages/Home';
 import ShareSpace from './Components/pages/ShareSpace'
 import MovieDrinkMatch from "./Components/pages/MovieDrinkMatch";
 import CodeQuiz from "./Components/pages/CodeQuiz";
+import WeatherDashboard from "./Components/pages/WeatherDashboard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/sharespace' component={ShareSpace} />
         <Route exact path='/moviedrinkmatch' component={MovieDrinkMatch} />
         <Route exact path='/codequiz' component={CodeQuiz} />
+        <Route exact path='/weatherdashboard' component={WeatherDashboard} />
       </div>
     </Router>
       
---
+++ src/Components/pages/WeatherDashboard.js
@@ -0,0 +1,15 @@
+import Header from '../Header/index.js'
+import WeatherDashboard from '../WeatherDashboard/index.js';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <WeatherDashboard />
+    </div>
+      
+
+  );
+}
+
+export default Home;
